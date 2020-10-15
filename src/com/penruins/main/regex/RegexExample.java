57c4874b3f2c4376d25b5ccb19453b4566963865
Penruins <lx_tdcq_king@icloud.com>
2020-10-15 12:25:07 +0800

    添加了一个正则表达式的例子

---
+++ src/com/penruins/main/regex/RegexExample.java
@@ -0,0 +1,12 @@
+package com.penruins.main.regex;
+
+import java.util.regex.Pattern;
+
+public class RegexExample {
+    public static void main(String[] args) {
+        String content = "I am noob from runoob.com";
+        String pattern = ".*runoob.*";
+        boolean isMatch = Pattern.matches(pattern, content);
+        System.out.println("字符串是否包含了 'runoob' 子字符串？" + isMatch);
+    }
+}
